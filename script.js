687f247d587d3a6fc3a76e48b831a6acaf9dfbdd
Manoj Saini <manojsaini513@gmail.com>
2023-06-15 10:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-// complete the given function
-
-function palindrome(str){
-	let rev_str = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        rev_str += str[i];
-    }
-    return rev_str;
+function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
 }
-function is_palindrome(str) {
-    reverse_str = reverse(str);
-    //  condition checking if reverse str is
-    // same as string it is palindrome
-    // else not a palindrome
-    if (reverse_str === str) {
-        console.log(true);
-    }
-    else {
-        console.log(false)
-    }
-
-}
-module.exports = palindrome
