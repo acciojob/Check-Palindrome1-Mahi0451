f34feeae8f3fc0d3257a0559f411ee38a69e340b
Manoj Saini <manojsaini513@gmail.com>
2023-06-15 10:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-function palindrome(str) {
- var re = /[^A-Za-z0-9]/g;
- str = str.toLowerCase().replace(re, '');
- var len = str.length;
- for (var i = 0; i < len/2; i++) {
-   if (str[i] !== str[len - 1 - i]) {
-       return false;
-   }
- }
- return true;
+function check_palindrome(str) {
+    let j = str.length - 1;
+    for (let i = 0; i < j / 2; i++) {
+        let x = str[i];//forward character
+        let y = str[j - i];//backward character
+        if (x != y) {
+            // return false if string not match
+            return false;
+        }
+    }
+    /// return true if string is palindrome
+    return true;
+ 
+}
+ 
+//function that print output if string is palindrome
+function is_palindrome(str) {
+    // variable that is true if string is palindrome
+    let ans = check_palindrome(str);
+    //condition checking ans is true or not
+    if (ans == true) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
 }
